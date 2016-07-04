87377e9ef53fdac399b24902008e3f1e0c5b881a
AlfGuerrero <guerrero.alfrancis@gmail.com>
2016-07-04 17:28:24 -0400

    Merge branch 'Ultraso'

--- V1_Haptic/V1_Haptic.ino
+++ V1_Haptic/V1_Haptic.ino
@@ -1,39 +1,29 @@
-<<<<<<< HEAD
-#define ECHOPIN 11                            // Pin to receive echo pulse
-#define TRIGPIN 12                           // Pin to send trigger pulse
-#include "pitches.h"
-// notes in the melody:
-#include <NewPing.h>
-=======
+/* Name: Heewon Suh
+ * Date: 06/02/2016
+ * Notes: This program uses haptic feedback to communicate distance.
+ */ 
+
 #define ECHOPIN 2                            // Pin to receive echo pulse
 #define TRIGPIN 3                            // Pin to send trigger pulse
 #include "pitches.h"
 // notes in the melody:
->>>>>>> Ultraso
+int melody[] = {
+  NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4
+};
 
 int finalMelody = NOTE_C4;
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-<<<<<<< HEAD
   4, 4, 4, 4, 4, 4, 4, 4
 };
 
 void setup() {
-=======
-  4, 4, 4, 4,4,4,4,4 };
-
-void setup(){
->>>>>>> Ultraso
   Serial.begin(9600);
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN, OUTPUT);
 }
 
-<<<<<<< HEAD
 void loop() {
-=======
-void loop(){
->>>>>>> Ultraso
   long distanceCm, distanceIn;
   digitalWrite(TRIGPIN, LOW);                   // Set the trigger pin to low for 2uS
   delayMicroseconds(2);
@@ -41,7 +31,6 @@ void loop(){
   delayMicroseconds(10);
   digitalWrite(TRIGPIN, LOW);                   // Send pin low again
   int distance = pulseIn(ECHOPIN, HIGH);        // Read in times pulse
-<<<<<<< HEAD
 
   // convert the time into a distance
   distanceCm = distance / 29.1 / 2 ;
@@ -49,7 +38,7 @@ void loop(){
 
   //duration of note
   int noteDuration = 1000 / distance;
-  tone(8, finalMelody, noteDuration);
+  //tone(8, finalMelody, noteDuration);
 
   // to distinguish the notes, set a minimum time between them.
   // the note's duration + 30% seems to work well:
@@ -57,27 +46,7 @@ void loop(){
   delay(pauseBetweenNotes);
   // stop the tone playing:
   noTone(8);
-
-
   if (distanceCm <= 0) {
-=======
-  
-  // convert the time into a distance
-  distanceCm = distance / 29.1 / 2 ;
-  distanceIn = distance / 74 / 2;
-  
-  //duration of note
-  int noteDuration = 1000/distance;
-  //tone(8, finalMelody, noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(8);
-  if (distanceCm <= 0){
->>>>>>> Ultraso
     Serial.println("Out of range");
   }
   else {
